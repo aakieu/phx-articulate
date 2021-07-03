c7a067a5abefd1b20678e3104357fc93f80eaa36
Aary Kieu <AaryKieu@gmail.com>
2021-07-03 13:12:20 -0700

    to test functions from other modules

---
+++ main-test.py
@@ -0,0 +1,36 @@
+from ax12_control.Ax12 import Ax12
+import math
+import phx
+from Joint import Joint
+
+# create Joint Objects
+waist = Joint([1], 'Waist')
+shoulder = Joint([2, 3], 'Shoulder')
+elbow = Joint([4, 5], 'Elbow')
+wristPitch = Joint([6], 'WristPitch')
+gripper = Joint([7], 'Gripper')
+
+# all Joint objects
+jointObjs = [waist, shoulder, elbow, wristPitch, gripper]
+
+CONNECT = True
+WINDOWS = False
+
+if CONNECT:
+    if WINDOWS: Ax12.DEVICENAME = 'COM3'
+    Ax12.connect()
+    setSpeedRaw(jointObjs,[100, 100, 100, 100, 100])
+
+    # map angles
+    waist.setMinMaxTheta(512)
+    shoulder.setMinMaxTheta(761)
+    # elbow.setMinMaxTheta()
+    # wristPitch.setMinMaxTheta()
+    # wristRoll.setMinMaxTheta()
+
+    # hardware test
+    #shoulder.setPositionAngle()
+
+
+    # getMotorPositions(jointObjs)
+    # Ax12.disconnect()
